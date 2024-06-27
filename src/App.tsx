ab40bd1d2b7740ba57a24dd316691b16bcdc78e7
kiheang <leeki1501@gmail.com>
2024-06-27 10:01:02 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-   return <div className="App">asdasdasdasd</div>;
+   return (
+      <div className="App">
+         <ul>
+            <li>asdasd</li>
+         </ul>
+      </div>
+   );
 }
 
 export default App;
